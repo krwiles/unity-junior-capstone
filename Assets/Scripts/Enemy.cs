213d2c3c1d8b94e491f337e890be4d552e8befb5
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2026-02-26 13:03:32 -0500

    feat: basic enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,34 @@
+#nullable enable
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    [SerializeField] private PathNode? _target;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _reachRadius;
+
+
+    public void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_target == null) return;
+        
+        Vector3 differenceToTarget = _target.gameObject.transform.position - transform.position;
+        
+        // check if target is reached
+        if (differenceToTarget.sqrMagnitude <= _reachRadius * _reachRadius)
+        {
+            _target = _target.GetNextNode(); // set next node
+            Move();
+        }
+        else
+        {
+            transform.Translate(differenceToTarget.normalized * _speed * Time.deltaTime);
+        }
+    }
 }
